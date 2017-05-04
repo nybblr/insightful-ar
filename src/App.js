a958b45ed1aec6d6c1f5f16da909a3e07c1a612b
Jonathan Martin <me@nybblr.com>
2017-05-04 13:51:14 -0400

    Deploy

--- src/App.js
+++ src/App.js
@@ -52,19 +52,35 @@ class App extends Component {
 
 
   render() {
-    let { notes } = this.state;
     return (
-      <div className="App">
-        <a-scene embedded artoolkit='sourceType: webcam;'>
-          <a-box position='0 0 0.5' material='opacity: 0.5;'></a-box>
-          <a-marker-camera preset='hiro'></a-marker-camera>
-        </a-scene>
-        {notes.map(note =>
-          <p key={note}>{note}</p>
-        )}
-      </div>
+      <a-scene embedded artoolkit='detectionMode: mono_and_matrix; matrixCodeType: 3x3; sourceType: webcam;'>
+        <a-marker type="barcode" value="11" barcodeValue="11">
+          <a-box depth="1" height="1" width="1" position='0 0.5 0' material='opacity: 0.5; side:double; color:green;'></a-box>
+        </a-marker>
+
+        <a-marker type="barcode" value="7" barcodeValue="7">
+          <a-box depth="1" height="1" width="1" position='0 0.5 0' material='opacity: 0.5; side:double; color:red;'></a-box>
+        </a-marker>
+
+        <a-marker type="barcode" value="3" barcodeValue="3">
+          <a-box depth="1" height="1" width="1" position='0 0.5 0' material='opacity: 0.5; side:double; color:blue;'></a-box>
+        </a-marker>
+
+        <a-entity camera></a-entity>
+      </a-scene>
     );
   }
 }
 
 export default App;
+
+
+
+    // let { notes } = this.state;
+        // {notes.map(note =>
+        //   <p key={note}>{note}</p>
+        // )}
+        // <a-scene embedded artoolkit='sourceType: webcam;'>
+        //   <a-box position='0 0 0.5' material='opacity: 0.5;'></a-box>
+        //   <a-marker-camera preset='hiro'></a-marker-camera>
+        // </a-scene>
