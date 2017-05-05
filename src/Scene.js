c3b264c7d8ac33eba0cf3a18cb1b90c7abbbc8fb
Jonathan Martin <me@nybblr.com>
2017-05-05 10:32:54 -0400

    Deploy

--- src/Scene.js
+++ src/Scene.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export default ({ users }) =>
-  <a-scene embedded artoolkit='detectionMode: mono_and_matrix; matrixCodeType: 3x3; sourceType: webcam;'>
+  <a-scene embedded vr-mode-ui="enabled: false" artoolkit='detectionMode: mono_and_matrix; matrixCodeType: 3x3; sourceType: webcam;'>
     { users.map(user =>
       <a-marker key={user.id} type="barcode" value={user.markerId} barcodeValue={user.markerId}>
         <a-image src={`./progress/${user.progress}.png`} depth="1" height="1" width="1" rotation="90 0 0" position="0 0.01 0"></a-image>
