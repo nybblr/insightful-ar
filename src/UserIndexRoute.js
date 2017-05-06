c54bc38096c42950fff84e948b45af3b92b2481b
Jonathan Martin <me@nybblr.com>
2017-05-05 22:11:28 -0400

    Deploy

--- src/UserIndexRoute.js
+++ src/UserIndexRoute.js
@@ -6,13 +6,18 @@ import Progress from './Progress';
 import t from './translations';
 import base from './database';
 
-let skill = 3;
+window.db = base;
 
 class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      user: {}
+      user: {},
+      pdfStat: {
+        numPages: 1,
+        pageNumber: 0,
+        sections: []
+      }
     };
   }
 
@@ -22,20 +27,33 @@ class App extends Component {
       context: this,
       state: 'user'
     });
+    this.ref2 = base.syncState(`/users/${params.id}/pdfStat`, {
+      context: this,
+      state: 'pdfStat'
+    });
   }
   componentDidMount(){
     this.init();
   }
   componentWillUnmount(){
     base.removeBinding(this.ref);
+    base.removeBinding(this.ref2);
   }
   componentWillReceiveProps(){
     base.removeBinding(this.ref);
+    base.removeBinding(this.ref2);
     this.init();
   }
 
   render() {
-    let { user } = this.state;
+    let { user, pdfStat } = this.state;
+    let { numPages, pageNumber, sections } = pdfStat;
+    let progress = pageNumber / numPages;
+    sections = sections.map(s => s
+      .replace(/Chapter/g, 'Ch.')
+      .replace(/Part/g, 'Pt.')
+    )
+
     return (
       <div className="modal animated fadeInUp">
         <Link to="/" className="close">
@@ -43,8 +61,8 @@ class App extends Component {
         </Link>
         <h1 className="name">{user.name}</h1>
         <h2 className="progress">{t.progress['on-track']}</h2>
-        <h2 className="progress">Book Progress: 78%</h2>
-        <Progress value={0.8} />
+        <h2 className="progress">Book Progress: {Math.round(progress*100)}%</h2>
+        <Progress value={progress} />
         <section className="row">
           <h3>Role:</h3>
           <p>{user.background || '—'}</p>
@@ -52,6 +70,12 @@ class App extends Component {
         <section className="row">
           <h3>Goals:</h3>
           <p>{user.goal || '—'}</p>
+        </section>
+        <section className="row">
+          <h3>Current Section:</h3>
+          <ul className="outline">{sections.map((section, i) =>
+            <li key={i} className={`animated delayed fadeInUp level-${i}`}>{section}</li>
+          )}</ul>
         </section>
         <section className="row">
           <h3>Topics:</h3>
